117d7356b48b5c605cd182d3bf77827142425e8f
Nina Glaser <nina.gl (at) web.de>
2013-04-10 12:11:42 +0200

    Kunde und Adresse

--- shop/src/main/java/de/shop/kundenverwaltung/domain/AbstractKunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/AbstractKunde.java
@@ -2,6 +2,7 @@ package de.shop.kundenverwaltung.domain;
 
 import java.io.Serializable;
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
@@ -22,9 +23,18 @@ public abstract class AbstractKunde implements Serializable {
 	public static final String FIRMENKUNDE = "F";
 	
 	private Long id;
+	private String vorname;
 	private String nachname;
+	private Date geburtsdatum;
+	private String geschlecht;
 	private String email;
+	private Date Eintrittsdatum;
+	private Date Aktualisiert;
+	
 	private Adresse adresse;
+	
+	
+	
 	@JsonIgnore
 	private List<Bestellung> bestellungen;
 	private URI bestellungenUri;
@@ -40,6 +50,36 @@ public abstract class AbstractKunde implements Serializable {
 	}
 	public void setNachname(String nachname) {
 		this.nachname = nachname;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public Date getGeburtsdatum() {
+		return geburtsdatum;
+	}
+	public void setGeburtsdatum(Date geburtsdatum) {
+		this.geburtsdatum = geburtsdatum;
+	}
+	public String getGeschlecht() {
+		return geschlecht;
+	}
+	public void setGeschlecht(String geschlecht) {
+		this.geschlecht = geschlecht;
+	}
+	public Date getEintrittsdatum() {
+		return Eintrittsdatum;
+	}
+	public void setEintrittsdatum(Date eintrittsdatum) {
+		Eintrittsdatum = eintrittsdatum;
+	}
+	public Date getAktualisiert() {
+		return Aktualisiert;
+	}
+	public void setAktualisiert(Date aktualisiert) {
+		Aktualisiert = aktualisiert;
 	}
 	public String getEmail() {
 		return email;
@@ -70,7 +110,24 @@ public abstract class AbstractKunde implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result
+				+ ((Aktualisiert == null) ? 0 : Aktualisiert.hashCode());
+		result = prime * result
+				+ ((Eintrittsdatum == null) ? 0 : Eintrittsdatum.hashCode());
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result
+				+ ((bestellungen == null) ? 0 : bestellungen.hashCode());
+		result = prime * result
+				+ ((bestellungenUri == null) ? 0 : bestellungenUri.hashCode());
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((geburtsdatum == null) ? 0 : geburtsdatum.hashCode());
+		result = prime * result
+				+ ((geschlecht == null) ? 0 : geschlecht.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((nachname == null) ? 0 : nachname.hashCode());
+		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
 		return result;
 	}
 	
@@ -83,18 +140,72 @@ public abstract class AbstractKunde implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		AbstractKunde other = (AbstractKunde) obj;
+		if (Aktualisiert == null) {
+			if (other.Aktualisiert != null)
+				return false;
+		} else if (!Aktualisiert.equals(other.Aktualisiert))
+			return false;
+		if (Eintrittsdatum == null) {
+			if (other.Eintrittsdatum != null)
+				return false;
+		} else if (!Eintrittsdatum.equals(other.Eintrittsdatum))
+			return false;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (bestellungen == null) {
+			if (other.bestellungen != null)
+				return false;
+		} else if (!bestellungen.equals(other.bestellungen))
+			return false;
+		if (bestellungenUri == null) {
+			if (other.bestellungenUri != null)
+				return false;
+		} else if (!bestellungenUri.equals(other.bestellungenUri))
+			return false;
 		if (email == null) {
 			if (other.email != null)
 				return false;
-		}
-		else if (!email.equals(other.email))
+		} else if (!email.equals(other.email))
+			return false;
+		if (geburtsdatum == null) {
+			if (other.geburtsdatum != null)
+				return false;
+		} else if (!geburtsdatum.equals(other.geburtsdatum))
+			return false;
+		if (geschlecht == null) {
+			if (other.geschlecht != null)
+				return false;
+		} else if (!geschlecht.equals(other.geschlecht))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nachname == null) {
+			if (other.nachname != null)
+				return false;
+		} else if (!nachname.equals(other.nachname))
+			return false;
+		if (vorname == null) {
+			if (other.vorname != null)
+				return false;
+		} else if (!vorname.equals(other.vorname))
 			return false;
 		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "AbstractKunde [id=" + id + ", nachname=" + nachname
-				+ ", email=" + email + ", bestellungenUri=" + bestellungenUri + "]";
+		return "AbstractKunde [id=" + id + ", vorname=" + vorname
+				+ ", nachname=" + nachname + ", geburtsdatum=" + geburtsdatum
+				+ ", geschlecht=" + geschlecht + ", email=" + email
+				+  ", adresse=" + adresse + ", bestellungen="
+				+ bestellungen + ", bestellungenUri=" + bestellungenUri + "]";
 	}
+
+
 }
--- shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
@@ -1,14 +1,18 @@
 package de.shop.kundenverwaltung.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 
 public class Adresse implements Serializable {
 	private static final long serialVersionUID = -3029272617931844501L;
 	private Long id;
+	private String strasse;
+	private Long hausnr;
 	private String plz;
 	private String ort;
+	private Date aktualisiert;
 	
 	@JsonIgnore
 	private AbstractKunde kunde;
@@ -19,6 +23,25 @@ public class Adresse implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	public Long getHausnr() {
+		return hausnr;
+	}
+	public void setHausnr(Long hausnr) {
+		this.hausnr = hausnr;
+	}
+	public Date getAktualisiert() {
+		return aktualisiert;
+	}
+	public void setAktualisiert(Date aktualisiert) {
+		this.aktualisiert = aktualisiert;
+	}
+
 	public String getPlz() {
 		return plz;
 	}
@@ -42,9 +65,14 @@ public class Adresse implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result
+				+ ((aktualisiert == null) ? 0 : aktualisiert.hashCode());
+		result = prime * result + ((hausnr == null) ? 0 : hausnr.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((kunde == null) ? 0 : kunde.hashCode());
 		result = prime * result + ((ort == null) ? 0 : ort.hashCode());
 		result = prime * result + ((plz == null) ? 0 : plz.hashCode());
+		result = prime * result + ((strasse == null) ? 0 : strasse.hashCode());
 		return result;
 	}
 	@Override
@@ -56,29 +84,47 @@ public class Adresse implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Adresse other = (Adresse) obj;
+		if (aktualisiert == null) {
+			if (other.aktualisiert != null)
+				return false;
+		} else if (!aktualisiert.equals(other.aktualisiert))
+			return false;
+		if (hausnr == null) {
+			if (other.hausnr != null)
+				return false;
+		} else if (!hausnr.equals(other.hausnr))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		}
-		else if (!id.equals(other.id))
+		} else if (!id.equals(other.id))
+			return false;
+		if (kunde == null) {
+			if (other.kunde != null)
+				return false;
+		} else if (!kunde.equals(other.kunde))
 			return false;
 		if (ort == null) {
 			if (other.ort != null)
 				return false;
-		}
-		else if (!ort.equals(other.ort))
+		} else if (!ort.equals(other.ort))
 			return false;
 		if (plz == null) {
 			if (other.plz != null)
 				return false;
-		}
-		else if (!plz.equals(other.plz))
+		} else if (!plz.equals(other.plz))
+			return false;
+		if (strasse == null) {
+			if (other.strasse != null)
+				return false;
+		} else if (!strasse.equals(other.strasse))
 			return false;
 		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "Adresse [id=" + id + ", plz=" + plz + ", ort=" + ort + "]";
+		return "Adresse [id=" + id + ", strasse=" + strasse + ", hausnr="
+				+ hausnr + ", plz=" + plz + ", ort=" + ort + ", kunde=" + kunde + "]";
 	}
 }
