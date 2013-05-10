d67a88c0891edce176ba96510b045dc70c94a281
Nina Glaser <nina.gl (at) web.de>
2013-05-10 14:02:57 +0200

    Attributwerte implementiert

--- shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellposten.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellposten.java
@@ -1,5 +1,9 @@
 package de.shop.bestellverwaltung.domain;
 
+import java.net.URI;
+
+import de.shop.artikelverwaltung.domain.Artikel;
+
 
 public class Bestellposten {
 
@@ -7,8 +11,10 @@ public class Bestellposten {
 	private Long menge;
 	private Long version;
 	private Long zwischenpreis;
-	private Bestellung bestellungId;
-	
+	private Bestellung bestellung;
+	private Artikel artikel;
+	private URI bestellungUri;
+	private URI artikelUri;
 	
 	
 	public Long getPositionId() {
@@ -36,19 +42,48 @@ public class Bestellposten {
 		this.zwischenpreis = zwischenpreis;
 	}
 	public Bestellung getBestellungId() {
-		return bestellungId;
+		return bestellung;
 	}
 	public void setBestellungId(Bestellung bestellungsId) {
-		bestellungId = bestellungsId;
+		bestellung = bestellungsId;
 	}
+	public Bestellung getBestellung() {
+		return bestellung;
+	}
+	public void setBestellung(Bestellung bestellung) {
+		this.bestellung = bestellung;
+	}
+	public Artikel getArtikel() {
+		return artikel;
+	}
+	public void setArtikel(Artikel artikel) {
+		this.artikel = artikel;
+	}
+	public URI getBestellungUri() {
+		return bestellungUri;
+	}
+	public void setBestellungUri(URI bestellungUri) {
+		this.bestellungUri = bestellungUri;
+	}
+	public URI getArtikelUri() {
+		return artikelUri;
+	}
+	public void setArtikelUri(URI artikelUri) {
+		this.artikelUri = artikelUri;
+	}	
+
 	
 	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((artikel == null) ? 0 : artikel.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((menge == null) ? 0 : menge.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
 		result = prime * result
-				+ ((id == null) ? 0 : id.hashCode());
+				+ ((zwischenpreis == null) ? 0 : zwischenpreis.hashCode());
 		return result;
 	}
 	@Override
@@ -59,20 +94,51 @@ public class Bestellposten {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		final Bestellposten other = (Bestellposten) obj;
+		Bestellposten other = (Bestellposten) obj;
+		if (artikel == null) {
+			if (other.artikel != null)
+				return false;
+		} else if (!artikel.equals(other.artikel))
+			return false;
+		if (artikelUri == null) {
+			if (other.artikelUri != null)
+				return false;
+		} else if (!artikelUri.equals(other.artikelUri))
+			return false;
+		if (bestellungUri == null) {
+			if (other.bestellungUri != null)
+				return false;
+		} else if (!bestellungUri.equals(other.bestellungUri))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		}
-		else if (!id.equals(other.id))
+		} else if (!id.equals(other.id))
+			return false;
+		if (menge == null) {
+			if (other.menge != null)
+				return false;
+		} else if (!menge.equals(other.menge))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		if (zwischenpreis == null) {
+			if (other.zwischenpreis != null)
+				return false;
+		} else if (!zwischenpreis.equals(other.zwischenpreis))
 			return false;
 		return true;
 	}
 	@Override
 	public String toString() {
-		return "Bestellposten [Positions_ID=" + id + ", Menge="
-				+ menge + ", Version=" + version + ", Zwischenpreis="
-				+ zwischenpreis + ", Bestellung_ID=" + bestellungId + "]";
-	}	
+		return "Bestellposten [id=" + id + ", menge=" + menge + ", version="
+				+ version + ", zwischenpreis=" + zwischenpreis
+				+ ", bestellung=" + bestellung + ", artikel=" + artikel
+				+  "]";
+	}
+
 }
 
