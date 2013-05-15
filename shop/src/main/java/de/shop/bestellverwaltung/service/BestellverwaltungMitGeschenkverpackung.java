654580f9c38b976a78419ccab9445f55427d42af
Benjamin Kupper <kube1017(at)hs-karlsruhe.de>
2013-05-15 13:52:13 +0200

    Bestellung ohne abstract

---
+++ shop/src/main/java/de/shop/bestellverwaltung/service/BestellverwaltungMitGeschenkverpackung.java
@@ -0,0 +1,45 @@
+package de.shop.bestellverwaltung.service;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+
+import de.shop.bestellverwaltung.domain.Bestellung;
+
+@Decorator
+public abstract class BestellverwaltungMitGeschenkverpackung implements
+		BestellungService {
+
+	@Inject
+	@Delegate
+	@Any
+	private BestellungService bestellverwaltung;
+
+	//public AbstractBestellverwaltungMitGeschenkverpackung() {
+		// TODO Auto-generated constructor stub
+//	}
+
+	@Override
+	public Bestellung findBestellungById(Long id, Locale locale) {
+		// TODO Auto-generated method stub
+		return bestellverwaltung.findBestellungById(id, locale);
+	}
+
+	@Override
+	public List<Bestellung> findBestellungenByKundeId(Long kundeId, Locale locale) {
+		// TODO Auto-generated method stub
+		return bestellverwaltung.findBestellungenByKundeId(kundeId, locale);
+	}
+
+	@Override
+	public Bestellung createBestellung(Bestellung bestellung,
+			 Locale locale) {
+		// TODO Auto-generated method stub
+		return bestellverwaltung.createBestellung(bestellung, locale);
+	}
+
+}
