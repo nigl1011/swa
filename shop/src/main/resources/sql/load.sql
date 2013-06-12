d25c877be3f3b455e07e1d28f37027be8d741222
Nina Glaser <nina.gl (at) web.de>
2013-06-12 12:16:05 +0200

    Sql statments eingefügt für alles Klassen

--- shop/src/main/resources/sql/load.sql
+++ shop/src/main/resources/sql/load.sql
@@ -25,3 +25,86 @@ INSERT INTO kunde_hobby (kunde_fk, hobby_fk) VALUES (6,2);
 INSERT INTO wartungsvertrag (nr, datum, inhalt, kunde_fk, idx, erzeugt, aktualisiert) VALUES (1,'31.01.2005','Wartungsvertrag_1_K101',1,0,'01.08.2006 00:00:00','01.08.2006 00:00:00');
 INSERT INTO wartungsvertrag (nr, datum, inhalt, kunde_fk, idx, erzeugt, aktualisiert) VALUES (2,'31.01.2006','Wartungsvertrag_2_K101',2,1,'02.08.2006 00:00:00','02.08.2006 00:00:00');
 INSERT INTO wartungsvertrag (nr, datum, inhalt, kunde_fk, idx, erzeugt, aktualisiert) VALUES (1,'30.06.2006','Wartungsvertrag_1_K102',3,0,'03.08.2006 00:00:00','03.08.2006 00:00:00');
+
+
+INSERT INTO bestellung (id, kunde_fk, idx, erzeugt, aktualisiert) VALUES (400,101,0,'01.08.2006 00:00:00','01.08.2006 00:00:00');
+INSERT INTO bestellung (id, kunde_fk, idx, erzeugt, aktualisiert) VALUES (401,101,1,'02.08.2006 00:00:00','02.08.2006 00:00:00');
+INSERT INTO bestellung (id, kunde_fk, idx, erzeugt, aktualisiert) VALUES (402,102,0,'03.08.2006 00:00:00','03.08.2006 00:00:00');
+INSERT INTO bestellung (id, kunde_fk, idx, erzeugt, aktualisiert) VALUES (403,102,1,'04.08.2006 00:00:00','04.08.2006 00:00:00');
+INSERT INTO bestellung (id, kunde_fk, idx, erzeugt, aktualisiert) VALUES (404,104,0,'05.08.2006 00:00:00','05.08.2006 00:00:00');
+
+INSERT INTO lieferung (id, liefernr, transport_art_fk, erzeugt, aktualisiert) VALUES (600,'20051005-001',0,'01.08.2006 00:00:00','01.08.2006 00:00:00');
+INSERT INTO lieferung (id, liefernr, transport_art_fk, erzeugt, aktualisiert) VALUES (601,'20051005-002',1,'02.08.2006 00:00:00','02.08.2006 00:00:00');
+INSERT INTO lieferung (id, liefernr, transport_art_fk, erzeugt, aktualisiert) VALUES (602,'20051005-003',2,'03.08.2006 00:00:00','03.08.2006 00:00:00');
+INSERT INTO lieferung (id, liefernr, transport_art_fk, erzeugt, aktualisiert) VALUES (603,'20051008-001',3,'04.08.2006 00:00:00','04.08.2006 00:00:00');
+
+
+INSERT INTO bestellung_lieferung (bestellung_fk, lieferung_fk) VALUES (400,600);
+INSERT INTO bestellung_lieferung (bestellung_fk, lieferung_fk) VALUES (401,600);
+INSERT INTO bestellung_lieferung (bestellung_fk, lieferung_fk) VALUES (402,601);
+INSERT INTO bestellung_lieferung (bestellung_fk, lieferung_fk) VALUES (402,602);
+INSERT INTO bestellung_lieferung (bestellung_fk, lieferung_fk) VALUES (403,602);
+INSERT INTO bestellung_lieferung (bestellung_fk, lieferung_fk) VALUES (404,603);
+
+
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (500,400,300,1,0);
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (501,400,301,4,1);
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (502,401,302,5,0);
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (503,402,303,3,0);
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (504,402,304,2,1);
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (505,403,305,1,0);
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (506,404,300,5,0);
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (507,404,301,2,1);
+INSERT INTO bestellposten (id, bestellung_fk, artikel_fk, anzahl, idx) VALUES (508,404,302,8,2);
+
+
+INSERT INTO artikel (id, bezeichnung, preis, ausgesondert, erzeugt, aktualisiert) VALUES (300,'Tisch ''Oval''',80,0,'01.08.2006 00:00:00','01.08.2006 00:00:00');
+INSERT INTO artikel (id, bezeichnung, preis, ausgesondert, erzeugt, aktualisiert) VALUES (301,'Stuhl ''Sitz bequem''',10,0,'02.08.2006 00:00:00','02.08.2006 00:00:00');
+INSERT INTO artikel (id, bezeichnung, preis, ausgesondert, erzeugt, aktualisiert) VALUES (302,'Tür ''Hoch und breit''',300,0,'03.08.2006 00:00:00','03.08.2006 00:00:00');
+INSERT INTO artikel (id, bezeichnung, preis, ausgesondert, erzeugt, aktualisiert) VALUES (303,'Fenster ''Glasklar''',150,0,'04.08.2006 00:00:00','04.08.2006 00:00:00');
+INSERT INTO artikel (id, bezeichnung, preis, ausgesondert, erzeugt, aktualisiert) VALUES (304,'Spiegel ''Mach mich schöner''',60,0,'05.08.2006 00:00:00','05.08.2006 00:00:00');
+INSERT INTO artikel (id, bezeichnung, preis, ausgesondert, erzeugt, aktualisiert) VALUES (305,'Kleiderschrank ''Viel Platz''',500,0,'06.08.2006 00:00:00','06.08.2006 00:00:00');
+INSERT INTO artikel (id, bezeichnung, preis, ausgesondert, erzeugt, aktualisiert) VALUES (306,'Bett ''Mit Holzwurm''',600,0,'07.08.2006 00:00:00','07.08.2006 00:00:00');
+
+
+CREATE INDEX adresse__kunde_index ON adresse(kunde_fk);
+CREATE INDEX kunde_hobby__kunde_index ON kunde_hobby(kunde_fk);
+CREATE INDEX wartungsvertrag__kunde_index ON wartungsvertrag(kunde_fk);
+CREATE INDEX bestellung__kunde_index ON bestellung(kunde_fk);
+CREATE INDEX bestpos__bestellung_index ON bestellposition(bestellung_fk);
+CREATE INDEX bestpos__artikel_index ON bestellposition(artikel_fk);
+
+
+DROP TABLE geschlecht;
+CREATE TABLE geschlecht(id NUMBER(1) NOT NULL PRIMARY KEY, txt VARCHAR2(10) NOT NULL UNIQUE) CACHE;
+INSERT INTO geschlecht VALUES (0, 'MAENNLICH');
+INSERT INTO geschlecht VALUES (1, 'WEIBLICH');
+
+DROP TABLE familienstand;
+CREATE TABLE familienstand(id NUMBER(1) NOT NULL PRIMARY KEY, txt VARCHAR2(12) NOT NULL UNIQUE) CACHE;
+INSERT INTO familienstand VALUES(0, 'LEDIG');
+INSERT INTO familienstand VALUES(1, 'VERHEIRATET');
+INSERT INTO familienstand VALUES(2, 'GESCHIEDEN');
+INSERT INTO familienstand VALUES(3, 'VERWITWET');
+
+DROP TABLE hobby;
+CREATE TABLE hobby(id NUMBER(1) NOT NULL PRIMARY KEY, txt VARCHAR2(16) NOT NULL UNIQUE) CACHE;
+INSERT INTO hobby VALUES (0, 'SPORT');
+INSERT INTO hobby VALUES (1, 'LESEN');
+INSERT INTO hobby VALUES (2, 'REISEN');
+
+DROP TABLE transport_art;
+CREATE TABLE transport_art(id NUMBER(1) NOT NULL PRIMARY KEY, txt VARCHAR2(8) NOT NULL UNIQUE) CACHE;
+INSERT INTO transport_art VALUES (0, 'STRASSE');
+INSERT INTO transport_art VALUES (1, 'SCHIENE');
+INSERT INTO transport_art VALUES (2, 'LUFT');
+INSERT INTO transport_art VALUES (3, 'WASSER');
+
+-- ===============================================================================
+-- Fremdschluessel in den bereits *generierten* Tabellen auf die obigen "Enum-Tabellen" anlegen
+-- ===============================================================================
+ALTER TABLE kunde ADD CONSTRAINT kunde__geschlecht_fk FOREIGN KEY (geschlecht_fk) REFERENCES geschlecht;
+ALTER TABLE kunde ADD CONSTRAINT kunde__familienstand_fk FOREIGN KEY (familienstand_fk) REFERENCES familienstand;
+ALTER TABLE kunde_hobby ADD CONSTRAINT kunde_hobby__hobby_fk FOREIGN KEY (hobby_fk) REFERENCES hobby;
+ALTER TABLE lieferung ADD CONSTRAINT lieferung__transport_art_fk FOREIGN KEY (transport_art_fk) REFERENCES transport_art;
+
